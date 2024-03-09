ae9b62b123752942e95fc6456caa57f67c6ca47b
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-03-09 02:54:49 +0100

    chore: change package from main to unconvert

--- unconvert.go
+++ unconvert.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Unconvert removes redundant type conversions from Go packages.
-package main
+// Package unconvert Unconvert removes redundant type conversions from Go packages.
+package unconvert
 
 import (
 	"bytes"
